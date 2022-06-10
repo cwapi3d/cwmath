3306c2e8a04299824a3f292c0d414a78b8f4154c
Jean-Sebastien Paquet <paquet@cadwork.ca>
2022-06-10 08:58:20 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,30 @@
+# Cadwork Math Utilities
+
+This is the Cadwork Math Utilities library in Python.
+
+## Installation
+
+Install with pip:
+
+```bash
+  pip install cwmath
+```
+    
+## Contributing
+
+Contributions are always welcome!
+  
+## Roadmap
+
+- Guides
+- Examples
+- API Documentation
+  
+## Authors
+
+- [@jspaquet](https://github.com/jspaquet)
+- [@Brunner246](https://github.com/Brunner246)
+  
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='cwmath',
+    version='0.0.2',
+    author='Cadwork',
+    author_email='it@cadwork.ca',
+    description='Cadwork Math Utilities',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/cwapi3d/cwmath',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries',
+    ],
+    python_requires='>=3.9, <4',
+    packages=find_packages(where="src"),
+    package_dir={'': 'src'},
+    package_data={
+        '': ['*.pyi'],
+    },
+    include_package_data=True,
+)
