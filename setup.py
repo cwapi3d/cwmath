4e028b258df3def9e8fefa475632163309be0e62
Jean-Sebastien Paquet <paquet@cadwork.ca>
2022-07-20 14:26:05 -0400

    Increments version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='cwmath',
-    version='0.0.2',
+    version='0.0.3',
     author='Cadwork',
     author_email='it@cadwork.ca',
     description='Cadwork Math Utilities',
